2a727a04196f0bf3584a3445cbbac9cef9c9ef2b
Melanie Bezot-Trottetfr <melanie@bigmouthfrog.fr>
2021-06-09 15:48:13 +0200

    react setup

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+
+const App = () => {
+  return (
+    <>
+              <img src={logo} className="App-logo" alt="Frog Posters" />
+    </>
+  )
+}
+
+export default App;
